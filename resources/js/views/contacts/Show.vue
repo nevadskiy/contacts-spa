f9f6901f082f3197bb1138e4e0ebbad2de77df7e
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-18 00:24:22 +0300

    WIP show contact view

---
+++ resources/js/views/contacts/Show.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contact: null,
+    };
+  },
+
+  created() {
+    window.axios.get(`/api/contacts/${this.$route.params.id}`)
+      .then((response) => {
+        this.contact = response.data.data;
+      })
+      .catch((errors) => {
+        console.log(errors);
+      });
+  },
+};
+</script>
