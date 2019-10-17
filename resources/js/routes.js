aae195cfafe29ea13e86033b877ef97867c7cb63
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-18 00:23:33 +0300

    Fix CS & add show contact view

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,19 +1,25 @@
 import Home from './views/Home.vue';
 import ContactsCreate from './views/contacts/Create.vue';
+import ContactsShow from './views/contacts/Show.vue';
 
 export default {
-    mode: 'history',
+  mode: 'history',
 
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/contacts/create',
-            name: 'contacts.create',
-            component: ContactsCreate,
-        }
-    ],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/contacts/create',
+      name: 'contacts.create',
+      component: ContactsCreate,
+    },
+    {
+      path: '/contacts/:id',
+      name: 'contacts.show',
+      component: ContactsShow,
+    },
+  ],
 };
