6e9d3bf3a058342057a086b4c4c25c78ba6781fe
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-18 00:23:55 +0300

    Add validation errors

--- resources/js/components/AppInput.vue
+++ resources/js/components/AppInput.vue
@@ -5,11 +5,14 @@
         <input
             :id="name"
             :type="type"
-            class="w-full py-2 text-gray-800 border-b border-gray-400 outline-none bg-transparent focus:border-blue-400"
             :placeholder="placeholder"
             :value="value"
+            class="w-full py-2 text-gray-800 border-b outline-none bg-transparent focus:border-blue-400"
+            :class="inputClasses"
             @input="update"
         >
+
+        <span v-show="shouldShowErrors" class="text-red-600 text-sm">{{ errors[0] }}</span>
     </div>
 </template>
 
@@ -39,11 +42,54 @@ export default {
       type: String,
       default: 'text',
     },
+
+    errors: {
+      type: Array,
+      default: () => [],
+    },
+  },
+
+  data() {
+    return {
+      isErrorsHidden: false,
+    };
+  },
+
+  computed: {
+    shouldShowErrors() {
+      return !this.isErrorsHidden && this.hasErrors;
+    },
+
+    hasErrors() {
+      return this.errors.length;
+    },
+
+    inputClasses() {
+      return {
+        'border-gray-400': !this.shouldShowErrors,
+        'border-red-600': this.shouldShowErrors,
+      };
+    },
+  },
+
+  watch: {
+    errors() {
+      this.showErrors();
+    },
   },
 
   methods: {
     update(event) {
+      this.hideErrors();
       this.$emit('input', event.target.value);
+    },
+
+    hideErrors() {
+      this.isErrorsHidden = true;
+    },
+
+    showErrors() {
+      this.isErrorsHidden = false;
     },
   },
 };
