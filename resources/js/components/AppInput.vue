a49f673246cfa451f5b2fefd53df84bf5acb22a3
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-17 22:34:00 +0300

    Add input component

---
+++ resources/js/components/AppInput.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <label :for="name" class="text-blue-500 uppercase font-bold">{{ label }}</label>
+
+        <input
+            :id="name"
+            :type="type"
+            class="w-full py-2 text-gray-800 border-b border-gray-400 outline-none bg-transparent focus:border-blue-400"
+            :placeholder="placeholder"
+            :value="value"
+            @input="update"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    label: {
+      type: String,
+      required: true,
+    },
+
+    placeholder: {
+      type: String,
+      default: '',
+    },
+
+    value: {
+      default: null,
+    },
+
+    type: {
+      type: String,
+      default: 'text',
+    },
+  },
+
+  methods: {
+    update(event) {
+      this.$emit('input', event.target.value);
+    },
+  },
+};
+</script>
