f02970c7c418cb3cd2b1b2f5a481069ae3bfe35c
Vitalii Kalaida <nevadskiy@gmail.com>
2019-10-18 23:24:17 +0300

    Feature avatar component

---
+++ resources/js/components/AppAvatar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        class="w-10 h-10 flex justify-center items-center border border-gray-300 rounded-full text-xl text-white bg-blue-400 leading-none shadow"
+        :title="name"
+    >
+        {{ initials }}
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    default: {
+      type: String,
+      default: 'U',
+    },
+  },
+
+  computed: {
+    initials() {
+      return this.name.match(/[A-Z]/g).slice(0, 2).join('') || this.default;
+    },
+  },
+};
+</script>
